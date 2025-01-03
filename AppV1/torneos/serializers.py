f0839a176c7e1c9939a576bf68860b23f283ae24
LuisZepeda <unicorniatech@gmail.com>
2025-01-03 14:37:04 -0600

    Esto contiene el requirements.txt, y algunos cambios en torneos y usuarios

--- AppV1/torneos/serializers.py
+++ AppV1/torneos/serializers.py
@@ -5,3 +5,11 @@ class TorneoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Torneo
         fields = '__all__'
+    def validate_tags(self, value):
+        if len(value) > 3:
+            raise serializers.ValidationError("No puedes seleccionar más de 3 tags.")
+        tags_nombres = [tag.nombre for tag in value]
+        if "Amateur" in tags_nombres and "Profesional" in tags_nombres:
+            raise serializers.ValidationError('No puedes seleccionar "Amateur" y "Profesional" al mismo tiempo.')
+
+        return value
--- AppV1/usuarios/models.py
+++ AppV1/usuarios/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Usuario(models.Model):
     nombre_completo = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
-    rating_inicial = models.DecimalField(max_digits=6, decimal_places=2)  # Puedes ajustar según el formato del rating
+    rating_inicial = models.DecimalField(max_digits=6, decimal_places=2, blank=True, null=True)  # Puedes ajustar según el formato del rating
     club = models.CharField(max_length=255, blank=True, null=True)  # Opcional: permite valores en blanco o nulos
     createdU = models.DateTimeField(auto_now_add=True,verbose_name="Creado") #Para saber cuanto tiempo lleva Creado
     modifiedU = models.DateTimeField(auto_now=True, verbose_name="Modificado") #Para saber última modificación
